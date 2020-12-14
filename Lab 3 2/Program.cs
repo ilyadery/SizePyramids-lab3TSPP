96f889f117849be7b5045ba2afb8bc18f94b4253
ilyadery <ilyadrbn@gmail.com>
2020-12-14 12:46:46 +0200

    Добавьте файлы проекта.

---
+++ Lab 3 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, h;
+            string Str;
+            Console.Write("Введите значение основания: ");
+            Str = Console.ReadLine();
+            a = Convert.ToInt16(Str);
+            Console.Write("Введите значение высоты: ");
+            Str = Console.ReadLine();
+            h = Convert.ToInt16(Str);
+            double v = (1 / 3) * Math.Pow(h * a, 2);
+            Console.WriteLine("Объём равен: " + v);
+            Console.ReadKey();
+           
+        }
+    }
+}
